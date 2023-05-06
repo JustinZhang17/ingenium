1e540dd59271d89fbab93179ef070df56d242fdc
Elias Hawa <elias.hawa2468@gmail.com>
2023-05-06 18:22:53 -0400

    Add TTS

--- src/components/atoms/textInput.tsx
+++ src/components/atoms/textInput.tsx
@@ -18,6 +18,7 @@ const TextInput = ({ id }: TextInputProps) => {
                 id="text-area"
                 className="peer h-full min-h-[100px] w-11/12 resize-none rounded-[7px] border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-50 bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-50"
                 placeholder="Ask Terry a question..."
+                maxLength={500}
             ></textarea>
         </>
     );
